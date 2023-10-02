efd661aca6bd58a1b4c0c4285aa2490a68868c19
Yijia <zeng.yi.jia@hotmail.com>
2023-10-02 11:56:34 +0200

    correct url adress to have no empty json and add try catch

--- exo1_requestapi.py
+++ exo1_requestapi.py
@@ -1,4 +1,35 @@
 import requests
-import json
-url="https://icanhazdadjoke.com/api"
+
+url="https://icanhazdadjoke.com"
 response=requests.get(url)
+try:
+    response = requests.get(url, headers={"Accept": "application/json"})
+
+    
+    if response.status_code == 200:
+        data = response.json()
+        joke = data["joke"]
+        print("Dad Joke:")
+        print(joke)
+    else:
+        print(f"API request failed with status code {response.status_code}")
+
+except requests.exceptions.RequestException as e:
+    print(f"API request error: {e}")
+
+except requests.exceptions.ConnectionError as e:
+    print(f"Connection error: {e}")
+
+except requests.exceptions.Timeout as e:
+    print(f"Request timed out: {e}")
+
+
+except requests.exceptions.HTTPError as e:
+    print(f"HTTP error: {e}")
+
+
+
+
+
+
+
