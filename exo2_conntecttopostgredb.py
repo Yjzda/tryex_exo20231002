a5db988aacdd5f78f7c5c5c8c7bcf74f0036a6ef
Yjzda <97908371+Yjzda@users.noreply.github.com>
2024-05-16 10:11:36 +0200

    Update exo2_conntecttopostgredb.py

--- exo2_conntecttopostgredb.py
+++ exo2_conntecttopostgredb.py
@@ -1,7 +1,7 @@
 import psycopg2
 import random
 
-# Connection parameters
+
 db_params = {
     "host": "localhost",  # The Docker host name
     "port": 5432,         # The port mapped to the PostgreSQL container in the Docker Compose file
@@ -13,18 +13,15 @@ db_params = {
 
 
 try:
-    # Create a connection to the PostgreSQL database
+  
     conn = psycopg2.connect(**db_params)
     print(conn)
 
-    # Create a cursor object
     cursor = conn.cursor()
     print(cursor)
 
-    # Execute a basic SQL query
     cursor.execute("SELECT * FROM table_exo2_postgre_docker")
 
-    # Fetch and print the results
     rows = cursor.fetchall()
     for row in rows:
         print(row)
@@ -36,7 +33,7 @@ except psycopg2.Error as e:
     print(f"Database error: {e}")
 
 finally:
-    # Close the cursor and connection in the 'finally' block to ensure they are always closed
+   
     if 'cursor' in locals():
         cursor.close()
     if 'conn' in locals():
